afba1916ddf7b48601287e15001cb4a835ddc21d
mkrasnauska <margaritakrasnauska@gmail.com>
2022-01-16 18:16:16 +0000

    1st JavaScript assignment
    1st JavaScript assignment

---
+++ basic javascript projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,23 @@
+alert(" Have a nice day!");   //creating window alert
+function my_first_function () {  //defining function and naming it
+    var str= "Rudolf the rednose raindeer";  //defining a variable and giving a string value
+    var result= str.fontcolor ("red");       //fontcolor on string variable
+    document.getElementById ("red_text").innerHTML= result;  // Putting value into HTML element 
+}
+var pets="Family", guineapig="Kenny", dog1="Aisha", dog2="Jojo";    //defining a variable
+document.write (dog1) ;   // display variable requested
+document.write ("<br>");  //placing text in new line 
+var guineapig="I like greens and celery";   //defining a variable
+var guineapig= guineapig.fontcolor("yellow");   //defining font color
+document.write (guineapig);  // display variable requested
+document.write ("<br>");  //placing text in new line 
+var dog1="I eat chicken and love to zoom around and not the skype like thing";  //defining a variable
+var dog1= dog1.fontcolor("blue");  //defining font color
+document.write (dog1);  // display variable requested
+document.write ("<br>");  //placing text in new line 
+var dog2="i eat anything i can find and poop a lot";  //defining a variable
+var dog2= dog2.fontcolor("green");  //defining font color
+document.write (dog2);  // display variable requested
+document.write ("<br>");  //placing text in new line 
+document.write (7-3);   // display variable requested
+document.write ("<br>");  //placing text in new line 
