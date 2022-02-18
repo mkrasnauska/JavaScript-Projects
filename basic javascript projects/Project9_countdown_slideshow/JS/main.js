302a2d33d56084be6aa1fc2eb06f53ac5b7ec5c7
mkrasnauska <margaritakrasnauska@gmail.com>
2022-02-18 17:45:35 +0000

    assignment #9

---
+++ basic javascript projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,52 @@
+var slideIndex = 1;    // defining a variable
+showSlides(slideIndex);
+
+// Next/prev controls
+function plusSlides(n) {   // defining function
+  showSlides(slideIndex += n);
+}
+
+// image controls
+function currentSlide(n) {   // defining function
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {    // defining function
+  var i;          // defining variable
+  var slides = document.getElementsByClassName("mySlides");   // defining variable
+  var dots = document.getElementsByClassName("dot");   // defining variable 
+  if (n > slides.length) {slideIndex = 1}     // assigning action to the if statement
+  if (n < 1) {slideIndex = slides.length}   // assigning action to the if statement
+  for (i = 0; i < slides.length; i++) {     // assigning action to the if statement
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {      // assigning action to the for statement
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";   // display
+  dots[slideIndex-1].className += " active";      // display
+}
+
+
+
+
+
+
+
+
+
+
+function countdown() {
+    var seconds=document.getElementById("seconds").ariaValueMax;
+    function tick() {
+        seconds=seconds-1;
+        TimeRanges.innerHTML=seconds;
+        setTimeout (tick,1000);
+        if (seconds==-1) {
+            alert ("Time's up");
+        }
+    }
+    tick();
+}
+
+
